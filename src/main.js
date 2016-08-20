d56b4fcbbbf8bbaf7f94df2ab74d524258150358
Jimmy Ranger <jranger@me.com>
2016-08-20 15:25:59 -0400

    a little cleanup

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
-// main.js
 import './main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-let groceries = [
-  { id: 1, name: "Oranges" },
-  { id: 2, name: "Bananas" },
-  { id: 3, name: "Bread" }
-];
-
 ReactDOM.render(
-  <App groceries={groceries} />,
+  <App />,
   document.getElementById('app')
 );
